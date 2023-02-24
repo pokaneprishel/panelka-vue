2346b93b485eaa4a16e6f9a76e693a3e0d3a6d68
Danil Mazitov <krimskyxd@gmail.com>
2023-02-24 17:51:29 +0300

    fix - adding files for deployment

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
-git push -f git@github.com:pokaneprishel/pokaneprishel.github.io.git master
+# git push -f git@github.com:pokaneprishel/pokaneprishel.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:pokaneprishel/panelka-vue/.git master:gh-pages
 
 cd -
